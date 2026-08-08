705f14def640e2399c260b2e68657d457fdca62b
Amir Tatarsky <amir.tatarsky@gmail.com>
2026-08-08 14:21:16 +0300

    auto: update code

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,7 @@
 import { clsx, type ClassValue } from 'clsx'
 import { twMerge } from 'tailwind-merge'
 import { QuoteItem, QuoteStatus } from './types'
+import type { Lang } from './i18n'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
